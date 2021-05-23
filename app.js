8f7033ad07f568b83113950c238f9ace4ab52438
obada-naser <obadanaser135@gmail.com>
2021-05-23 15:44:34 +0300

    adding lab01

---
+++ app.js
@@ -0,0 +1,19 @@
+let userName=prompt("what is your name ?");
+alert('hello '+userName);
+console.log('hello '+userName);
+let userAge=prompt('how old are you'+' '+ userName+' ?'  );
+console.log("you are "+ userAge+ " !");
+let userHoppy=prompt("what is your favourite hoppy ?")
+console.log("your hoppy is "+ userHoppy);
+alert('WoW! what a nice hoppy '+userName);
+
+let workUser=prompt("what do you do for living "+ userName+ " ?");
+
+console.log("you work on "+ workUser +" !");
+alert("Interesting !");
+
+alert('So, Hello ' +userName+ ', you are '+ userAge +' years old. ');
+alert('your hoppy is '+ userHoppy + ' and your work is '+ workUser  );
+alert("Thank you very much !");
+
+
